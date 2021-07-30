978d203d21964a90e80f0a50be907890a5ec5657
Esteban Jimenez <yeisson310@hotmail.com>
2021-07-30 09:40:14 -0500

    Try to fix bug netlify, paths of components app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Header from "./components/Header";
-import TrendingSlider from "./components/TrendingSlider";
-import PopularSlider from "./components/PopularSlider";
-import HighestRatedSlider from "./components/HighestRatedSlider";
-import Footer from "./components/Footer";
+import Header from "@components/Header";
+import TrendingSlider from "@components/TrendingSlider";
+import PopularSlider from "@components/PopularSlider";
+import HighestRatedSlider from "@components/HighestRatedSlider";
+import Footer from "@components/Footer";
 
 const App = () => {
   return (
